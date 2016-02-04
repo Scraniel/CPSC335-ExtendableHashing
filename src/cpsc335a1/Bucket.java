7d26071177177ada23b429317fc223bc368aa4f9
Danny Lewis <nitomatik@gmail.com>
2016-02-04 15:51:22 -0700

    Fixed add / insert functions, added an empty() function. Next step: Test everything, then implement the hash table

--- src/cpsc335a1/Bucket.java
+++ src/cpsc335a1/Bucket.java
@@ -10,6 +10,7 @@ public class Bucket {
 	int numCharsLeft;
 	int firstOpenSlot = 0;
 	static final int DEFAULT_BUCKET_SIZE = 128;
+	StringBuilder builder;
 	
 	public Bucket(int size, int depth)
 	{
@@ -24,11 +25,12 @@ public class Bucket {
 		
 		numCharsLeft = slots.length;
 		localDepth = depth;
+		builder = new StringBuilder();
 	}
 	
 	// Returns the index of the start of the string, or -1 if the string isn't present.
-	// TODO: Right now just does a linear search, once the indices are sorted I can change
-	// 		 to a binary search.
+	// TODO: Right now just does a linear search. After testing the insert to make sure
+	// 		 elements are inserted in order, should update this to do binary search.
 	public int search(String toFind)
 	{
 		for(int index : indices)
@@ -53,26 +55,40 @@ public class Bucket {
 	}
 	
 	// Inserts a new index in the correct sorted order.
-	// TODO: Finish implementing!
-	private void insert(int index)
+	private void insert(int index, String adding)
 	{
 		if(indices.isEmpty())
 			indices.add(index);
 		
-		int current = indices.get(indices.size() / 2);
 		
-		StringBuilder builder = new StringBuilder();
-		
-		int length = slots[current];
-		int end = current + 1 + length;
-		
-		for(int i = current + 1; i < end; i++)
-			builder.append(slots[i]);
-		
-		
+		for(int i = 0; i < indices.size(); i++)
+		{	
+			String comparing = constructString(indices.get(i));
+			
+			if(comparing.compareTo(adding) > 0)
+			{
+				indices.add(i, index);
+				break;
+			}
+				
+		}
 		
 	}
 	
+	// Returns the string starting at index i in the bucket.
+	private String constructString(int index)
+	{	
+		int length = slots[index];
+		int end = index + 1 + length;
+		
+		for(int j = index + 1; j < end; j++)
+			builder.append(slots[j]);
+		
+		String constructed = builder.toString();
+		builder.setLength(0);
+		
+		return constructed;
+	}
 	
 	// Adds the string to the bucket. If the bucket isn't big enough to add the string,
 	// returns false. At this point the hash table should go about doing the doubling 
@@ -85,7 +101,7 @@ public class Bucket {
 		if(numCharsLeft < toAdd.length() + 1)
 			return false;
 		
-		
+		insert(firstOpenSlot, toAdd);
 		
 		slots[firstOpenSlot] = (char)toAdd.length();
 		for(int i = firstOpenSlot + 1; i <= toAdd.length(); i++)
@@ -96,6 +112,25 @@ public class Bucket {
 		
 		return true;
 	}
+	
+	public String[] empty()
+	{
+		String words[] = new String[indices.size()];
+		
+		int current = 0;
+		for(int i : indices)
+		{
+			words[current] = constructString(i);
+			current++;
+		}
+		
+		numCharsLeft = slots.length;
+		firstOpenSlot = 0;
+		indices.clear();
+		
+		return words;
+	}
+	
 	
 	// Deletes the requested word from the bucket. Returns false if the word could not
 	// be found. Automatically rearranges the words so the empty spaces are at the end
